c24d3569a6c2ac99715931c084000e83919c8d7c
afrinh <35729013+afrinh@users.noreply.github.com>
2018-07-13 14:49:37 +0530

    Add files via upload

---
+++ Week1/Day6/QueueWithTwoStacks.py
@@ -0,0 +1,72 @@
+import unittest
+
+
+class QueueTwoStacks(object):
+
+    # Implement the enqueue and dequeue methods
+    def __init__(self):
+        self.instack=[]
+        self.outstack=[]
+    
+
+    def enqueue(self, item):
+        self.instack.append(item)
+
+    def dequeue(self):
+        if not self.outstack:
+            while self.instack:
+                self.outstack.append(self.instack.pop())
+        return self.outstack.pop()
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_queue_usage(self):
+        queue = QueueTwoStacks()
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+
+        actual = queue.dequeue()
+        expected = 1
+        self.assertEqual(actual, expected)
+
+        actual = queue.dequeue()
+        expected = 2
+        self.assertEqual(actual, expected)
+
+        queue.enqueue(4)
+
+        actual = queue.dequeue()
+        expected = 3
+        self.assertEqual(actual, expected)
+
+        actual = queue.dequeue()
+        expected = 4
+        self.assertEqual(actual, expected)
+
+        with self.assertRaises(Exception):
+            queue.dequeue()
+
+
+unittest.main(verbosity=2)
